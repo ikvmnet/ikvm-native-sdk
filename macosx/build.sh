2c36df7b1029ea027eb32e400397236ac39b5371
Jerome Haltom <jhaltom@alethic.solutions>
2023-06-29 15:30:33 -0500

    Update build.sh
    Delete in build.

--- macosx/build.sh
+++ macosx/build.sh
@@ -5,3 +5,6 @@ mkdir -p $dist
 
 url="https://alethicdevops.blob.core.windows.net/artifacts/MacOSX11.1.sdk.tar.xz?sv=2021-08-06&st=2023-05-11T05%3A24%3A28Z&se=2026-05-12T05%3A24%3A00Z&sr=b&sp=r&sig=jwUrA375PbpNh7oVbIIDzG7OGA8DifZxHH1Egx6LmJs%3D"
 curl --fail -L $url | tar xJv -C $dist --strip-components=1
+
+rm -f $dist/System/Library/Frameworks/Ruby.framework/Versions/2.6/Headers/ruby/ruby
+rm -f $dist/System/Library/Frameworks/Ruby.framework/Headers/ruby/ruby/
